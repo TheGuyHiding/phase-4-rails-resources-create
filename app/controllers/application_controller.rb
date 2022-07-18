fe39ef56eb908d75217b3e8cb14a55946a62dd47
Luis Ibarra <luis9609@gmail.com>
2022-07-18 14:12:46 -0600

    commit tree 8296d352d57ba2d9e1f9835d76f0cb274cacb89d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    def show
+        bird = Bird.find_by(id: params[:id])
+        render json: bird
+    end
+
+    def create
+        bird = Bird.create(name: param[:id], species: params[:species])
+        render json: bird, status: :created
+    end
+
 end
